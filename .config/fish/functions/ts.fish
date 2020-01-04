3e201e2f5fe0329bc61277b7dd0c89719fbdf057
Coleman Nugent <colemannugent@gmail.com>
2020-01-03 16:19:07 -0800

    config/fish: Add function to quickly setup my dev environment

---
+++ .config/fish/functions/ts.fish
@@ -0,0 +1,11 @@
+function ts
+	tmux new-session -d -s falco-dev -n dev -c ~/src/Falco/
+	tmux new-window -d -t '=falco-dev' -n runner -c ~/src/Falco/
+	tmux send-keys -t '=falco-dev:=runner' 'dcu' Enter
+	tmux new-window -d -t '=falco-dev' -n logs -c ~/src/Falco/
+	tmux send-keys -t '=falco-dev:=logs' 'tail -f log/production.log' Enter
+	tmux new-window -d -t '=falco-dev' -c ~/src/Falco/
+	docker run --privileged -d -p 389:389 rroemhild/test-openldap
+	tmux attach
+end
+
