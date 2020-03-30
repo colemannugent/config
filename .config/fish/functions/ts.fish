2e78bb5eeb8809ccb32fcfc7ed4ca396a037d713
Coleman Nugent <colemannugent@gmail.com>
2020-03-30 11:48:28 -0700

    config: Use GRC to colorize Falco logs

--- .config/fish/functions/ts.fish
+++ .config/fish/functions/ts.fish
@@ -3,7 +3,7 @@ function ts
 	tmux new-window -d -t '=falco-dev' -n runner -c ~/src/Falco/
 	tmux send-keys -t '=falco-dev:=runner' 'dcu' Enter
 	tmux new-window -d -t '=falco-dev' -n logs -c ~/src/Falco/
-	tmux send-keys -t '=falco-dev:=logs' 'tail -f log/production.log' Enter
+	tmux send-keys -t '=falco-dev:=logs' 'grc tail -f log/production.log' Enter
 	tmux new-window -d -t '=falco-dev' -c ~/src/Falco/
 	docker run --privileged -d -p 389:389 rroemhild/test-openldap
 	tmux attach
